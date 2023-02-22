9824986760769b80142ee47ebce38a9eedc15cd7
riad40 <riadabdelaziz40@gmail.com>
2023-02-22 15:24:57 +0100

    read audio files from the device storage and list them on the library screen

--- app/screens/Library.js
+++ app/screens/Library.js
@@ -1,10 +1,42 @@
-import {Text, View} from 'react-native';
+import {Text, View, FlatList} from 'react-native';
+import {useState, useEffect} from 'react';
+import RNFS from 'react-native-fs';
 import styles from '../styles/appContainer';
+import libraryStyling from '../styles/libraryStyling';
+import useStoragePermission from '../hooks/useStoragePermission';
 
 function Library() {
+  const [songs, setSongs] = useState([]);
+
+  const getSongs = async () => {
+    try {
+      const permission = useStoragePermission();
+      permission();
+      const path = RNFS.ExternalStorageDirectoryPath + '/Music';
+      const files = await RNFS.readDir(path);
+      const songs = files.filter(file => file.name.endsWith('.mp3'));
+      setSongs(songs);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getSongs();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Library</Text>
+      <Text style={libraryStyling.heading}>Library</Text>
+
+      <FlatList
+        data={songs}
+        renderItem={({item}) => (
+          <View style={libraryStyling.songContainer}>
+            <Text style={libraryStyling.songTitle}>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
---
+++ app/styles/libraryStyling.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from 'react-native';
+
+const libraryStyling = StyleSheet.create({
+  heading: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  songContainer: {
+    padding: 10,
+    backgroundColor: '#343a40',
+    width: '100%',
+    borderColor: '#2AC062',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  songTitle: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default libraryStyling;
